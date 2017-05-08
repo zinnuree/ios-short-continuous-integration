ff0cbf6d1b45b881d3d41f6e79ef99450970c6e9
Nazmul Quader <nazmul@konasl.com>
2017-05-08 15:32:10 +0600

    add toRoman test

--- RomanNumeralCalculatorTests/RomanNumeralCalculatorTests.swift
+++ RomanNumeralCalculatorTests/RomanNumeralCalculatorTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import RomanNumeralCalculator
 
 class RomanNumeralCalculatorTests: XCTestCase {
     
@@ -20,9 +21,10 @@ class RomanNumeralCalculatorTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testToRoman() {
+        XCTAssertEqual(toRoman(1), "I")
+        XCTAssertEqual(toRoman(2), "II")
+        XCTAssertEqual(toRoman(4), "IV")
     }
     
     func testPerformanceExample() {
